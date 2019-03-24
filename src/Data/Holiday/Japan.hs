b72ac14f30529f3b67637b3452a06434c89ed039
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2019-03-24 23:58:48 +0900

    `Holiday` values in Japanese
    There are no official English names for Japanese holidays.

--- src/Data/Holiday/Japan.hs
+++ src/Data/Holiday/Japan.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE MultiWayIf #-}
 module Data.Holiday.Japan
   ( Holiday(..)
-  , toJapanese
+  , display
   , holiday
   , isHoliday
   ) where
@@ -13,58 +13,65 @@ import           Data.Time.Calendar.WeekDate (toWeekDate)
 
 -- | Data type for Japanese holidays.
 data Holiday
-  = NewYear'sDay
-  | ComingOfAgeDay
-  | NationalFoundationDay
-  | VernalEquinoxDay
-  | ShowaDay
-  | ConstitutionMemorialDay
-  | GreeneryDay
-  | Children'sDay
-  | MarineDay
-  | MountainDay
-  | RespectForTheAgedDay
-  | AutumnalEquinoxDay
-  | HealthAndSportsDay
-  | SportsDay
-  | CultureDay
-  | LabourThanksgivingDay
-  | Emperor'sBirthday
-  | NationalHoliday
-  | MakeUpHoliday
-  | CeremonialOfEnthronement
-  | RitesOfShowaEmperorFuneral
-  | CeremonialOfPrinceAkihito'sMarriage
-  | CeremonialOfPrinceNaruhito'sMarriage
-  | EnthronementDay
+  -- | New Year's Day
+  = D元日
+  -- | Coming of Age Day
+  | D成人の日
+  -- | National Foundation Day
+  | D建国記念の日
+  -- | Vernal Equinox Day
+  | D春分の日
+  -- | Showa Day
+  | D昭和の日
+  -- | Constitution Memorial Day
+  | D憲法記念日
+  -- | Greenery Day
+  | Dみどりの日
+  -- | Children's Day
+  | Dこどもの日
+  -- | Marine Day
+  | D海の日
+  -- | Mountain Day
+  | D山の日
+  -- | Respect for the Aged Day
+  | D敬老の日
+  -- | Autumnal Equinox Day
+  | D秋分の日
+  -- | Health and Sports Day
+  | D体育の日
+  -- | Sports Day
+  | Dスポーツの日
+  -- | Culture Day
+  | D文化の日
+  -- | Labour Thanksgiving Day
+  | D勤労感謝の日
+  -- | Emperor's Birthday
+  | D天皇誕生日
+  -- | National Holiday
+  | D国民の休日
+  -- | Make Up Holiday
+  | D振替休日
+  -- | Ceremonial of Enthronement
+  | D即位礼正殿の儀
+  -- | Rites of Showa Emperor Funeral
+  | D昭和天皇の大喪の礼
+  -- | Ceremonial of Prince Akihito's Marriage
+  | D皇太子明仁親王の結婚の儀
+  -- | Ceremonial of Prince Naruhito's Marriage
+  | D皇太子徳仁親王の結婚の儀
+  -- | Enthronement Day
+  | D即位の日
   deriving (Eq, Show)
 
--- | Return Japanese name of @Holiday@s.
-toJapanese :: Holiday -> String
-toJapanese NewYear'sDay                         = "元日"
-toJapanese ComingOfAgeDay                       = "成人の日"
-toJapanese NationalFoundationDay                = "建国記念の日"
-toJapanese VernalEquinoxDay                     = "春分の日"
-toJapanese ShowaDay                             = "昭和の日"
-toJapanese ConstitutionMemorialDay              = "憲法記念日"
-toJapanese GreeneryDay                          = "みどりの日"
-toJapanese Children'sDay                        = "こどもの日"
-toJapanese MarineDay                            = "海の日"
-toJapanese MountainDay                          = "山の日"
-toJapanese RespectForTheAgedDay                 = "敬老の日"
-toJapanese AutumnalEquinoxDay                   = "秋分の日"
-toJapanese HealthAndSportsDay                   = "体育の日"
-toJapanese SportsDay                            = "スポーツの日"
-toJapanese CultureDay                           = "文化の日"
-toJapanese LabourThanksgivingDay                = "勤労感謝の日"
-toJapanese Emperor'sBirthday                    = "天皇誕生日"
-toJapanese NationalHoliday                      = "国民の休日"
-toJapanese MakeUpHoliday                        = "振替休日"
-toJapanese CeremonialOfEnthronement             = "即位礼正殿の儀"
-toJapanese RitesOfShowaEmperorFuneral           = "昭和天皇の大喪の礼"
-toJapanese CeremonialOfPrinceAkihito'sMarriage  = "皇太子明仁親王の結婚の儀"
-toJapanese CeremonialOfPrinceNaruhito'sMarriage = "皇太子徳仁親王の結婚の儀"
-toJapanese EnthronementDay                      = "即位の日"
+-- | Remove prefix \"D\" and show 'Holiday' name.
+--
+-- >>> putStrLn $ display D元日
+-- 元日
+display :: Holiday -> String
+display h =
+  case show h of
+    ('D' : name) -> name
+    _            -> error "`Holiday` value is started by D"
 
 -- | Identify if the day is a holiday or not.
 isHoliday :: Day -> Bool
@@ -72,78 +79,78 @@ isHoliday = isJust . holiday
 
 -- | Identify which holiday the day is if possible.
 --
--- >>> putStrLn $ toJapanese $ Data.Maybe.fromJust $ holiday $ fromGregorian 2015 5 5
--- こどもの日
+-- >>> holiday $ fromGregorian 2015 5 5
+-- Just Dこどもの日
 --
 -- >>> holiday $ fromGregorian 2015 12 8
 -- Nothing
 holiday :: Day -> Maybe Holiday
 holiday day | day < enforcement = Nothing
 holiday day = case toGregorian day of
-  (_, 1, 1) -> Just NewYear'sDay
+  (_, 1, 1) -> Just D元日
   (y, 1, _)
-    | y >= 2000 && isNthMonday 2 day -> Just ComingOfAgeDay
-  (_, 1, 15) -> Just ComingOfAgeDay
+    | y >= 2000 && isNthMonday 2 day -> Just D成人の日
+  (_, 1, 15) -> Just D成人の日
   (y, 2, 11)
-    | y >= 1967 -> Just NationalFoundationDay
+    | y >= 1967 -> Just D建国記念の日
   (y, 2, 23)
-    | y >= 2020 -> Just Emperor'sBirthday
-  (1989, 2, 24) -> Just RitesOfShowaEmperorFuneral
+    | y >= 2020 -> Just D天皇誕生日
+  (1989, 2, 24) -> Just D昭和天皇の大喪の礼
   (y, 3, d)
-    | d == vernalEquinox y -> Just VernalEquinoxDay
-  (1959, 4, 10) -> Just CeremonialOfPrinceAkihito'sMarriage
+    | d == vernalEquinox y -> Just D春分の日
+  (1959, 4, 10) -> Just D皇太子明仁親王の結婚の儀
   (y, 4, 29)
-    | y >= 2007 -> Just ShowaDay
-    | y >= 1989 -> Just GreeneryDay
-    | otherwise -> Just Emperor'sBirthday
-  (2019, 4, 30) -> Just NationalHoliday
-  (2019, 5, 1) -> Just EnthronementDay
-  (2019, 5, 2) -> Just NationalHoliday
-  (_, 5, 3) -> Just ConstitutionMemorialDay
+    | y >= 2007 -> Just D昭和の日
+    | y >= 1989 -> Just Dみどりの日
+    | otherwise -> Just D天皇誕生日
+  (2019, 4, 30) -> Just D国民の休日
+  (2019, 5, 1) -> Just D即位の日
+  (2019, 5, 2) -> Just D国民の休日
+  (_, 5, 3) -> Just D憲法記念日
   (y, 5, 4)
-    | y >= 2007 -> Just GreeneryDay
-    | y >= 1986 && not (isSunday day) && not (isMonday day) -> Just NationalHoliday
-  (_, 5, 5) -> Just Children'sDay
+    | y >= 2007 -> Just Dみどりの日
+    | y >= 1986 && not (isSunday day) && not (isMonday day) -> Just D国民の休日
+  (_, 5, 5) -> Just Dこどもの日
   (y, 5, 6)
-    | y >= 2007 && (isTuesday day || isWednesday day) -> Just MakeUpHoliday
-  (1993, 6, 9) -> Just CeremonialOfPrinceNaruhito'sMarriage
-  (2020, 7, 23) -> Just MarineDay
-  (2020, 7, 24) -> Just SportsDay
+    | y >= 2007 && (isTuesday day || isWednesday day) -> Just D振替休日
+  (1993, 6, 9) -> Just D皇太子徳仁親王の結婚の儀
+  (2020, 7, 23) -> Just D海の日
+  (2020, 7, 24) -> Just Dスポーツの日
   (2020, 7, _) -> Nothing
   (y, 7, _)
-    | y >= 2003 && isNthMonday 3 day -> Just MarineDay
+    | y >= 2003 && isNthMonday 3 day -> Just D海の日
   (y, 7, 20)
-    | y >= 1996 -> Just MarineDay
-  (2020, 8, 10) -> Just MountainDay
+    | y >= 1996 -> Just D海の日
+  (2020, 8, 10) -> Just D山の日
   (2020, 8, _) -> Nothing
   (y, 8, 11)
-    | y >= 2016 -> Just MountainDay
+    | y >= 2016 -> Just D山の日
   (y, 9, d) -> let equinox = autumnalEquinox y
                in if d == equinox
-                  then Just AutumnalEquinoxDay
+                  then Just D秋分の日
                   else if y >= 2003
                        then if isNthMonday 3 day
-                            then Just RespectForTheAgedDay
+                            then Just D敬老の日
                             else if isTuesday day && d == equinox - 1
-                                 then Just NationalHoliday
+                                 then Just D国民の休日
                                  else Nothing
                        else if y >= 1966 && d == 15
-                            then Just RespectForTheAgedDay
+                            then Just D敬老の日
                             else Nothing
-  (2019, 10, 22) -> Just CeremonialOfEnthronement
+  (2019, 10, 22) -> Just D即位礼正殿の儀
   (y, 10, _)
     | y >= 2000 && isNthMonday 2 day -> if
       | y == 2020 -> Nothing
-      | y >= 2020 -> Just SportsDay
-      | otherwise -> Just HealthAndSportsDay
+      | y >= 2020 -> Just Dスポーツの日
+      | otherwise -> Just D体育の日
   (y, 10, 10)
-    | y >= 1966 -> Just HealthAndSportsDay
-  (_, 11, 3) -> Just CultureDay
-  (_, 11, 23) -> Just LabourThanksgivingDay
-  (1990, 11, 12) -> Just CeremonialOfEnthronement
+    | y >= 1966 -> Just D体育の日
+  (_, 11, 3) -> Just D文化の日
+  (_, 11, 23) -> Just D勤労感謝の日
+  (1990, 11, 12) -> Just D即位礼正殿の儀
   (y, 12, 23)
-    | y >= 1989 && y <= 2018 -> Just Emperor'sBirthday
-  _ | isMonday day && isHoliday (addDays (-1) day) -> Just MakeUpHoliday
+    | y >= 1989 && y <= 2018 -> Just D天皇誕生日
+  _ | isMonday day && isHoliday (addDays (-1) day) -> Just D振替休日
   _ -> Nothing
 
 enforcement :: Day
--- test/Data/Holiday/JapanSpec.hs
+++ test/Data/Holiday/JapanSpec.hs
@@ -5,7 +5,7 @@ import           Data.List          (intercalate)
 import           Data.Time.Calendar (fromGregorian)
 import           Test.Hspec         (Spec, describe, it, parallel, shouldBe)
 
-import           Data.Holiday.Japan (Holiday (AutumnalEquinoxDay, Children'sDay, ComingOfAgeDay, ConstitutionMemorialDay, CultureDay, Emperor'sBirthday, EnthronementDay, GreeneryDay, HealthAndSportsDay, LabourThanksgivingDay, MakeUpHoliday, MarineDay, MountainDay, NationalFoundationDay, NationalHoliday, NewYear'sDay, RespectForTheAgedDay, ShowaDay, SportsDay, VernalEquinoxDay),
+import           Data.Holiday.Japan (Holiday (Dこどもの日, Dみどりの日, Dスポーツの日, D体育の日, D元日, D勤労感謝の日, D即位の日, D国民の休日, D天皇誕生日, D山の日, D建国記念の日, D憲法記念日, D成人の日, D振替休日, D敬老の日, D文化の日, D春分の日, D昭和の日, D海の日, D秋分の日),
                                      holiday)
 
 spec :: Spec
@@ -14,24 +14,24 @@ spec = parallel $
     let
       holidays :: [(Integer, Int, Int, Holiday)]
       holidays =
-        [ (2015,  1,  1, NewYear'sDay)
-        , (2015,  1, 12, ComingOfAgeDay)
-        , (2015,  2, 11, NationalFoundationDay)
-        , (2015,  3, 21, VernalEquinoxDay)
-        , (2015,  4, 29, ShowaDay)
-        , (2015,  5,  3, ConstitutionMemorialDay)
-        , (2015,  5,  4, GreeneryDay)
-        , (2015,  5,  5, Children'sDay)
-        , (2015,  7, 20, MarineDay)
-        , (2015,  9, 21, RespectForTheAgedDay)
-        , (2015,  9, 22, NationalHoliday)
-        , (2015,  9, 23, AutumnalEquinoxDay)
-        , (2015, 10, 12, HealthAndSportsDay)
-        , (2015, 11,  3, CultureDay)
-        , (2015, 11, 23, LabourThanksgivingDay)
-        , (2015, 12, 23, Emperor'sBirthday)
-        , (2009,  5,  6, MakeUpHoliday)
-        , (2016,  8, 11, MountainDay)
+        [ (2015,  1,  1, D元日)
+        , (2015,  1, 12, D成人の日)
+        , (2015,  2, 11, D建国記念の日)
+        , (2015,  3, 21, D春分の日)
+        , (2015,  4, 29, D昭和の日)
+        , (2015,  5,  3, D憲法記念日)
+        , (2015,  5,  4, Dみどりの日)
+        , (2015,  5,  5, Dこどもの日)
+        , (2015,  7, 20, D海の日)
+        , (2015,  9, 21, D敬老の日)
+        , (2015,  9, 22, D国民の休日)
+        , (2015,  9, 23, D秋分の日)
+        , (2015, 10, 12, D体育の日)
+        , (2015, 11,  3, D文化の日)
+        , (2015, 11, 23, D勤労感謝の日)
+        , (2015, 12, 23, D天皇誕生日)
+        , (2009,  5,  6, D振替休日)
+        , (2016,  8, 11, D山の日)
         ]
 
     holidays `for_` \(year, month, day, holiday') ->
@@ -47,33 +47,33 @@ spec = parallel $
 
     describe "New Emperor's Birthday" $ do
       it "2018-12-23 is Emperor's Birthday" $
-        holiday (fromGregorian 2018 12 23) `shouldBe` Just Emperor'sBirthday
+        holiday (fromGregorian 2018 12 23) `shouldBe` Just D天皇誕生日
       it "2019-12-23 is not Emperor's Birthday" $
         holiday (fromGregorian 2019 12 23) `shouldBe` Nothing
       it "2019-02-23 is not Emperor's Birthday" $
         holiday (fromGregorian 2019 2 23) `shouldBe` Nothing
       it "2020-02-23 is Emperor's Birthday" $
-        holiday (fromGregorian 2020 2 23) `shouldBe` Just Emperor'sBirthday
+        holiday (fromGregorian 2020 2 23) `shouldBe` Just D天皇誕生日
 
     describe "Health and Sports Day is turend into Sports Day from 2020" $ do
       it "2019-10-14 is Health and Sports Day" $
-        holiday (fromGregorian 2019 10 14) `shouldBe` Just HealthAndSportsDay
+        holiday (fromGregorian 2019 10 14) `shouldBe` Just D体育の日
       it "2021-10-11 is Sports Day" $
-        holiday (fromGregorian 2021 10 11) `shouldBe` Just SportsDay
+        holiday (fromGregorian 2021 10 11) `shouldBe` Just Dスポーツの日
 
     describe "Tokyo Olympic" $ do
       it "2020-07-23 is Marine Day" $
-        holiday (fromGregorian 2020 7 23) `shouldBe` Just MarineDay
+        holiday (fromGregorian 2020 7 23) `shouldBe` Just D海の日
       it "2020-07-20 is not Marine Day" $
         holiday (fromGregorian 2020 7 20) `shouldBe` Nothing
 
       it "2020-07-24 is Sports Day" $
-        holiday (fromGregorian 2020 7 24) `shouldBe` Just SportsDay
+        holiday (fromGregorian 2020 7 24) `shouldBe` Just Dスポーツの日
       it "2020-10-12 is not Sports Day" $
         holiday (fromGregorian 2020 10 12) `shouldBe` Nothing
 
       it "2020-08-10 is Mountain Day" $
-        holiday (fromGregorian 2020 8 10) `shouldBe` Just MountainDay
+        holiday (fromGregorian 2020 8 10) `shouldBe` Just D山の日
       it "2020-08-11 is not Mountain Day" $
         holiday (fromGregorian 2020 8 11) `shouldBe` Nothing
 
@@ -81,28 +81,28 @@ spec = parallel $
       it "1986-05-04 is not National Holiday" $
         holiday (fromGregorian 1986 5 4) `shouldBe` Nothing
       it "1987-05-04 is not National Holiday" $
-        holiday (fromGregorian 1987 5 4) `shouldBe` Just MakeUpHoliday
+        holiday (fromGregorian 1987 5 4) `shouldBe` Just D振替休日
       it "1988-05-04 is National Holiday" $
-        holiday (fromGregorian 1988 5 4) `shouldBe` Just NationalHoliday
+        holiday (fromGregorian 1988 5 4) `shouldBe` Just D国民の休日
 
     describe "Golden Week of 2019" $ do
       it "2019-04-28 is not a holiday" $
         holiday (fromGregorian 2019 4 28) `shouldBe` Nothing
       it "2019-04-29 is Showa Day" $
-        holiday (fromGregorian 2019 4 29) `shouldBe` Just ShowaDay
+        holiday (fromGregorian 2019 4 29) `shouldBe` Just D昭和の日
       it "2019-04-30 is National Holiday" $
-        holiday (fromGregorian 2019 4 30) `shouldBe` Just NationalHoliday
+        holiday (fromGregorian 2019 4 30) `shouldBe` Just D国民の休日
       it "2019-05-01 is Enthronement Day" $
-        holiday (fromGregorian 2019 5 1) `shouldBe` Just EnthronementDay
+        holiday (fromGregorian 2019 5 1) `shouldBe` Just D即位の日
       it "2019-05-02 is National Holiday" $
-        holiday (fromGregorian 2019 5 2) `shouldBe` Just NationalHoliday
+        holiday (fromGregorian 2019 5 2) `shouldBe` Just D国民の休日
       it "2019-05-03 is Constitution Memorial Day" $
-        holiday (fromGregorian 2019 5 3) `shouldBe` Just ConstitutionMemorialDay
+        holiday (fromGregorian 2019 5 3) `shouldBe` Just D憲法記念日
       it "2019-05-04 is Greenery Day" $
-        holiday (fromGregorian 2019 5 4) `shouldBe` Just GreeneryDay
+        holiday (fromGregorian 2019 5 4) `shouldBe` Just Dみどりの日
       it "2019-05-05 is Children's Day" $
-        holiday (fromGregorian 2019 5 5) `shouldBe` Just Children'sDay
+        holiday (fromGregorian 2019 5 5) `shouldBe` Just Dこどもの日
       it "2019-05-06 is Make Up Holiday" $
-        holiday (fromGregorian 2019 5 6) `shouldBe` Just MakeUpHoliday
+        holiday (fromGregorian 2019 5 6) `shouldBe` Just D振替休日
       it "2019-05-07 is not a holiday" $
         holiday (fromGregorian 2019 5 7) `shouldBe` Nothing
